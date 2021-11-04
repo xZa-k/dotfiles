f6156e689a5f497404fa6e2e20c9a91d835e17e9
Zak <zakalex.body@gmail.com>
2021-11-04 23:03:21 +0000

    Fixed fish greeting

--- .config/omf/themes/zakfish/fish_greeting.fish
+++ .config/omf/themes/zakfish/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    command ufetch.sh
-    set quotes (cat ~/.config/quotes.txt)
+    command ./pfetch.sh
+    set quotes (cat ./quotes.txt)
     echo $quotes[(random 1 (count $quotes))]
 end
